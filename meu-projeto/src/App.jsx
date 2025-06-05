bc482e9f4ffb542aba9adbd79bd0240740185f29
Gabriel Fleury Martins <gabrielfleury34@gmail.com>
2025-06-04 23:38:25 -0300

    aprimorando

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,46 +1,52 @@
 import './App.css';
-import Condicional from './components/Condicional';
-/* import Frase from './components/Frase';
+/* import Condicional from './components/Condicional';
+import Frase from './components/Frase';
 import SayMainName from './components/SayMainName';
 import Pessoa from './components/Pessoa';
-import List from './components/List' */
+import List from './components/List' 
 import Evento from './components/Evento';
 import Form from './components/Form';
-import OutraLista from './components/OutraLista';
+import OutraLista from './components/OutraLista';*/
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 function App() {
-  
-
-  const meusItens = ['React', 'Vue', 'Angular']
-  /* const nome = 'Maria' */
+  const meusItens = ['React', 'Vue', 'Angular'];
 
   return (
-    <div className='logo'>
-      
-      <h1>Renderizaçao de Listas</h1>
-      <OutraLista itens={''}/>
+    <Router>
+      <Navbar/>
 
-
-
-
-
-
-
-      {/* <Condicional/> */}
-      {/* <Evento numero="1"/>
-      
-      <Form/> */}
-      {/* <Evento/>
-      <Frase/>
-      <SayMainName nome="Gabriel"/>
-      <SayMainName nome="Joao"/>
-      <SayMainName nome={nome}/>
-      <Pessoa nome="Rodrigo" idade="28" profissao="Programdor" foto="https://via.placeholder"/>
-      <List/>  */}
-      
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer/>
+    </Router>
   );
+      
+      
+
+
+    {/* <OutraLista itens={''}/> */}
+    {/* <Condicional/> */}
+    {/* <Evento numero="1"/>
+    <Form/> */}
+    {/* <Evento/>
+    <Frase/>
+     <SayMainName nome="Gabriel"/>
+    <SayMainName nome="Joao"/>
+    <SayMainName nome={nome}/>
+    <Pessoa nome="Rodrigo" idade="28" profissao="Programdor" foto="https:/via.placeholder"/>
+      List/>  */}
+    
+  
 }
 
 export default App; // Apenas o App é exportado aqui
